be1093a319c69eee40110e6329e980bf86362834
praveendwievedi <praveen240903@gmail.com>
2025-07-20 16:59:05 +0530

    create user model and connect db to the server

---
+++ src/main/java/com/fiitPeeps/userService/model/UserModel.java
@@ -0,0 +1,36 @@
+package com.fiitPeeps.userService.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalTime;
+
+@Entity
+@Data
+@Table(name = "users")
+public class UserModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String userId;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String firstName;
+    private String lastName;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole role= UserRole.USER;
+
+    @CreationTimestamp
+    private LocalTime createdAt;
+    @UpdateTimestamp
+    private LocalTime updatedAt;
+}
